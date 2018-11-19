2b551f1534f338809f853e6decce3a51b226223e
c0ban-project <39722780+c0ban-project@users.noreply.github.com>
2018-11-19 10:33:28 +0900

    Fix burn first block index

--- counterblock/lib/config.py
+++ counterblock/lib/config.py
@@ -37,6 +37,10 @@ DEFAULT_BACKEND_PORT = 3882
 DEFAULT_LOG_SIZE_KB = 20000
 DEFAULT_LOG_NUM_FILES = 5
 
+TESTNET_BLOCK_FIRST = 100
+MAINNET_BLOCK_FIRST = 1630117
+
+
 ##
 # STATE
 ##
@@ -76,9 +80,9 @@ def init_base(args):
     # first block
     global BLOCK_FIRST
     if TESTNET:
-        BLOCK_FIRST = 100
+        BLOCK_FIRST = TESTNET_BLOCK_FIRST
     else:
-        BLOCK_FIRST = 100
+        BLOCK_FIRST = MAINNET_BLOCK_FIRST
 
     global LATEST_BLOCK_INIT
     LATEST_BLOCK_INIT = {'block_index': BLOCK_FIRST, 'block_time': None, 'block_hash': None}
